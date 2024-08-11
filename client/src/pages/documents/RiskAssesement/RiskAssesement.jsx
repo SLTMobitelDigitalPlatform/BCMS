4c841ceac498871d658038b534b03e474e23a859
SupunJayaweera <jayaweera_st_e22@engug.ruh.ac.lk>
2024-08-11 12:19:59 +0530

    UI bug Fixing

--- client/src/pages/documents/RiskAssesement/RiskAssesement.jsx
+++ client/src/pages/documents/RiskAssesement/RiskAssesement.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Title from "../../../../components/Title";
-import ContextNavigation from "../../../../components/ContextNavigation";
-import Sidebar from "../../../../components/Sidebar";
+import Title from "../../../components/Title";
+import ContextNavigation from "../../../components/ContextNavigation";
+import Sidebar from "../../../components/Sidebar";
 
 const Objectives = () => {
   return (
