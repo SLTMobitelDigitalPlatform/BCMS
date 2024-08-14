041fca0203e947b4a3fdafb4488c392c4f0cdc41
SupunJayaweera <jayaweera_st_e22@engug.ruh.ac.lk>
2024-08-14 20:57:18 +0530

    Display logged in user details

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -15,7 +15,7 @@ const Welcome = () => {
             Authorization: `Bearer ${token}`,
           },
         });
-        console.log(response);
+
         setUser(response.data);
       } catch (error) {
         console.error("Error fetching user details:", error);
