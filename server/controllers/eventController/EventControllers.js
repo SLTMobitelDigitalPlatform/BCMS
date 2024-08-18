c8112405b577279f6f9c1792de605e35c79d45ed
SupunJayaweera <jayaweera_st_e22@engug.ruh.ac.lk>
2024-08-18 18:42:02 +0530

    integrating notifications to the event controller

--- server/controllers/eventController/EventControllers.js
+++ server/controllers/eventController/EventControllers.js
@@ -1,4 +1,14 @@
 const Event = require("../../models/eventModels/EventModels");
+const User = require("../../models/userModels/user");
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "sltmobitelbcmssystem@gmail.com",
+    pass: "lvbr ypwc atoa qcfi",
+  },
+});
 
 exports.getAllEvents = async (req, res) => {
   try {
@@ -11,8 +21,43 @@ exports.getAllEvents = async (req, res) => {
 
 exports.createEvent = async (req, res) => {
   try {
-    const newEvent = new Event(req.body);
+    const { title, start, end, describe, attendees } = req.body;
+
+    // Create a new event
+    const newEvent = new Event({ title, start, end, describe, attendees });
     await newEvent.save();
+
+    // Fetch emails and names of all attendees
+    const attendeeDetails = await User.find({
+      _id: { $in: attendees },
+    }).select("email name");
+
+    // Prepare the list of attendee names
+    const attendeeNames = attendeeDetails.map((att) => att.name).join(", ");
+
+    // Send individual emails to each attendee
+    for (const attendee of attendeeDetails) {
+      const mailOptions = {
+        from: "sltmobitelbcmssystem@gmail.com",
+        to: attendee.email,
+        subject: "New Event Scheduled",
+        text: `Dear ${attendee.name},\n\nYou are invited to the following event:\n
+               Title: ${title}\n
+               Date: ${start}\n
+               End: ${end}\n
+               Attendees: ${attendeeNames}\n\n
+               Note: This is an auto-generated email. Please do not reply to this email.`,
+      };
+
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log("Error sending email to:", attendee.email, error);
+        } else {
+          console.log("Email sent to:", attendee.email, info.response);
+        }
+      });
+    }
+
     res.status(201).json(newEvent);
   } catch (error) {
     res.status(500).json({ message: "Error creating event", error });
@@ -21,11 +66,51 @@ exports.createEvent = async (req, res) => {
 
 exports.updateEvent = async (req, res) => {
   try {
+    const { title, start, end, describe, attendees } = req.body;
+    // console.log(describe);
+
     const updatedEvent = await Event.findByIdAndUpdate(
       req.params.id,
-      req.body,
+      { title, start, end, describe, attendees },
       { new: true }
     );
+
+    if (!updatedEvent) {
+      return res.status(404).json({ message: "Event not found" });
+    }
+
+    // Fetch emails and names of all attendees
+    const attendeeDetails = await User.find({
+      _id: { $in: attendees },
+    }).select("email name");
+
+    // Prepare the list of attendee names
+    const attendeeNames = attendeeDetails.map((att) => att.name).join(", ");
+    // console.log(attendeeDetails);
+    // console.log(attendees);
+
+    // Send individual emails to each attendee
+    for (const attendee of attendeeDetails) {
+      const mailOptions = {
+        from: "sltmobitelbcmssystem@gmail.com",
+        to: attendee.email,
+        subject: "Event Updated",
+        text: `Dear ${attendee.name},\n\nThe details of the event have been updated:\n
+               Title: ${title}\n
+              Date: ${start}\n
+               End: ${end}\n
+               Attendees: ${attendeeNames}\n\n
+               Note: This is an auto-generated email. Please do not reply to this email.`,
+      };
+
+      try {
+        const info = await transporter.sendMail(mailOptions);
+        console.log("Email sent to:", attendee.email, info.response);
+      } catch (error) {
+        console.log("Error sending email to:", attendee.email, error);
+      }
+    }
+
     res.status(200).json(updatedEvent);
   } catch (error) {
     res.status(500).json({ message: "Error updating event", error });
@@ -34,7 +119,37 @@ exports.updateEvent = async (req, res) => {
 
 exports.deleteEvent = async (req, res) => {
   try {
-    await Event.findByIdAndDelete(req.params.id);
+    const event = await Event.findByIdAndDelete(req.params.id);
+
+    if (!event) {
+      return res.status(404).json({ message: "Event not found" });
+    }
+
+    // Fetch emails and names of all attendees
+    const attendeeDetails = await User.find({
+      _id: { $in: event.attendees },
+    }).select("email name");
+
+    // Send individual emails to each attendee
+    for (const attendee of attendeeDetails) {
+      const mailOptions = {
+        from: "sltmobitelbcmssystem@gmail.com",
+        to: attendee.email,
+        subject: "Event Cancelled",
+        text: `Dear ${attendee.name},\n\nThe event titled "${event.title}" scheduled on ${event.start} has been cancelled.\n
+               Description: ${event.describe}\n
+               Note: This is an auto-generated email. Please do not reply to this email.`,
+      };
+
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log("Error sending email to:", attendee.email, error);
+        } else {
+          console.log("Email sent to:", attendee.email, info.response);
+        }
+      });
+    }
+
     res.status(200).json({ message: "Event deleted successfully" });
   } catch (error) {
     res.status(500).json({ message: "Error deleting event", error });
--- server/models/eventModels/EventModels.js
+++ server/models/eventModels/EventModels.js
@@ -1,32 +1,37 @@
 const moment = require("moment");
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const EventSchema = new mongoose.Schema({
-    title: {
-        type: String, 
-        required: [true, "Please write a title for your event"]
+  title: {
+    type: String,
+    required: [true, "Please write a title for your event"],
+  },
+  start: {
+    type: Date,
+    required: [true, "Please Insert The Start of your event"],
+    min: [new Date(), "Event start date can't be before now!"],
+  },
+  end: {
+    type: Date,
+    // Setting a min function to accept any date one hour ahead of start
+    min: [
+      function () {
+        const date = new Date(this.start);
+        const validDate = new Date(date.setHours(date.getHours() + 1));
+        return validDate;
+      },
+      "Event end must be at least one hour ahead of event start time",
+    ],
+    default: function () {
+      const date = new Date(this.start);
+      return date.setDate(date.getDate() + 1);
     },
-    start: {
-        type: Date,
-        required: [true, "Please Insert The Start of your event"],
-        min: [new Date(), "Event start date can't be before now!"]
-    },
-    end: {
-        type: Date,
-        // Setting a min function to accept any date one hour ahead of start
-        min: [function() {
-            const date = new Date(this.start);
-            const validDate = new Date(date.setHours(date.getHours() + 1));
-            return validDate;
-        }, "Event end must be at least one hour ahead of event start time"],
-        default: function() {
-            const date = new Date(this.start);
-            return date.setDate(date.getDate() + 1);
-        }
-    },
-    describe: {
-        type: String
-    }
+  },
+  describe: {
+    type: String,
+  },
+  attendees: [{ type: Schema.Types.ObjectId, ref: "User" }],
 });
 
 module.exports = mongoose.model("Event", EventSchema);
