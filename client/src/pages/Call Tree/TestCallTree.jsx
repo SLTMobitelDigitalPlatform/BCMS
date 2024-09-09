9005161d2f539b2cf5c8d1b82fcc3299ff170e12
SupunJayaweera <jayaweera_st_e22@engug.ruh.ac.lk>
2024-09-09 17:47:39 +0530

    adding test org-chart for testing

--- client/src/pages/Call Tree/TestCallTree.jsx
+++ client/src/pages/Call Tree/TestCallTree.jsx
@@ -1,80 +1,70 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { Tree, TreeNode } from "react-organizational-chart";
+import React from "react";
+import { OrgDiagram } from "basicprimitivesreact";
+import { PageFitMode, Enabled } from "basicprimitives";
 
-// StyledNode component to display each node
-const StyledNode = ({ label, mobileNumber }) => (
-  <div className="p-2 rounded-lg inline-block border-2 border-indigo-600 text-center">
-    <div>{label}</div>
-    <div>{mobileNumber}</div>
-  </div>
-);
-
-// Recursive function to render TreeNode components
-const renderTreeNodes = (nodeData) => {
-  return (
-    <TreeNode
-      label={
-        <StyledNode
-          label={nodeData.label}
-          mobileNumber={nodeData.mobileNumber}
-        />
-      }
-    >
-      {nodeData.children &&
-        nodeData.children.map((child, index) => (
-          <TreeNode
-            key={index}
-            label={
-              <StyledNode
-                label={child.label}
-                mobileNumber={child.mobileNumber}
-              />
-            }
-          >
-            {child.children && renderTreeNodes(child)}
-          </TreeNode>
-        ))}
-    </TreeNode>
-  );
+var photos = {
+  a:
+    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAA8CAIAAACrV36WAAAAAXNSR0IArs4c6QAAAARn" +
+    "QU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAGnSURBVGhD7dnBbQJBDAVQk1o2QjlQwKYGzpSwKQfq4IxIC" +
+    "RTB9jLZHCJFwWv7/7EiDt6zmX2yPYMHNq01eb7n5flI36JiIXWpbFW2kAwgsdVblS0kA0hs9db/ZWs+vW/Wno9PxPE3dh" +
+    "ls6Od+HI1XT1d64Sb8R5utEulwdbA8VY+LZ/kqkfF456pBHxDz5Xxze/p2vsxukBbAshTVOE0PO4B2cUlWKrgUTKsrV0e" +
+    "ut3RVU/cm5aKKqPXVbjuIDPtDUh2JImq1+jmjkupIFNFStXadHncWXkecpb3393me4oJZnionXyjLV6W4QFZEleHCWNG+" +
+    "0eKggQJiRVV6vhAXwoqrul0AC1H1uuIsTLUyukYH1jBL7WJ8lgq6oqwkVXSQDrLSVEFXjJWoirlCrFRVyBVhJasirgCr6" +
+    "5tEv7a5A5jL0tcN7vNl9OVcHqtXRbocVr+Kc9k3H/3qPL69Ise7dh0SsS+2JmtFddgvdy/gGbY7Jdp2GRcyrlu1BfUjxt" +
+    "iPRm/lqVbGHOMHnU39zQm0I/UbBLA+GVosJHGVrcoWkgEktnoLydYXkF/LiXG21MwAAAAASUVORK5CYII=",
 };
 
-const TestCallTree = () => {
-  const [treeData, setTreeData] = useState([]);
-
-  const fetchCallTree = async () => {
-    try {
-      const response = await axios.get("http://localhost:5000/callTree");
-      setTreeData(response.data);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
+function TestCallTree() {
+  const config = {
+    pageFitMode: PageFitMode.AutoSize,
+    autoSizeMinimum: { width: 100, height: 100 },
+    cursorItem: 0,
+    highlightItem: 0,
+    hasSelectorCheckbox: Enabled.True,
+    items: [
+      {
+        id: 0,
+        parent: null,
+        title: "James Smith",
+        description: "VP, Public Sector",
+        image: photos.a,
+      },
+      {
+        id: 1,
+        parent: 0,
+        title: "Ted Lucas",
+        description: "VP, Human Resources",
+        image: photos.a,
+      },
+      {
+        id: 2,
+        parent: 0,
+        title: "Fritz Stuger",
+        description: "Business Solutions, US",
+        image: photos.a,
+      },
+      {
+        id: 3,
+        parent: 2,
+        title: "Ted Lucas",
+        description: "VP, Human Resources",
+        image: photos.a,
+      },
+      {
+        id: 4,
+        parent: 2,
+        title: "Fritz Stuger",
+        description: "Business Solutions, US",
+        image: photos.a,
+      },
+    ],
   };
 
-  useEffect(() => {
-    fetchCallTree();
-  }, []);
-
-  if (!treeData.length) return <p>Loading...</p>;
-
   return (
-    <div className="w-full h-full overflow-auto">
-      {treeData.map((node, index) => (
-        <Tree
-          key={index}
-          lineWidth={"2px"}
-          lineColor={"green"}
-          lineBorderRadius={"10px"}
-          label={
-            <StyledNode label={node.label} mobileNumber={node.mobileNumber} />
-          }
-        >
-          {node.children &&
-            node.children.map((child) => renderTreeNodes(child))}
-        </Tree>
-      ))}
+    <div className="App">
+      <OrgDiagram centerOnCursor={true} config={config} />
     </div>
   );
-};
+}
 
 export default TestCallTree;
