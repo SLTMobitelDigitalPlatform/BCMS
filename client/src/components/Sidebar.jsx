97398264da62ab379af57424efcbfce909bf10a9
Thamindu Dasanayake <136196555+thamindu2003@users.noreply.github.com>
2024-09-30 09:36:06 +0530

    Sidebar update

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,18 +1,26 @@
-import { Menu, Transition } from "@headlessui/react";
-import { Fragment } from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 import { useAuth } from "../auth/AuthContext";
-// import { ChevronDownIcon } from "@heroicons/react/20/solid";
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
+import { FaChevronDown } from "react-icons/fa";
 
 const Sidebar = () => {
+  const [documentDropdown, setDocumentDropdown] = useState(false);
+  const [managementDropdown, setManagementDropdown] = useState(false);
+
+  const toggleDocumentDropdown = () => {
+    setDocumentDropdown(!documentDropdown);
+  };
+
+  const toggleManagementDropdown = () => {
+    setManagementDropdown(!managementDropdown);
+  };
+
   const { logout } = useAuth();
   const handleLogout = async () => {
     try {
       // localStorage.removeItem("token");
+      localStorage.removeItem("scrollPosition");
+      localStorage.removeItem("carouselIndex");
       logout();
       window.location.href = "/login";
     } catch (error) {
@@ -20,156 +28,218 @@ const Sidebar = () => {
       alert("Failed to log out. Please try again.");
     }
   };
+
   return (
-    <div>
-      <div className="ml-5 rounded-2xl w-full bg-indigo-950 text-center p-5 flex flex-col justify-between">
-        <div>
-          <Link to="">
-            <h1 className="text-[40px] font-bold text-[#00BBF6]">BCMS</h1>
-          </Link>
-          {/* <h1 className="text-md text-[#C8ECFE] font-semibold">Dashboard</h1> */}
-        </div>
-        <div className="px-7 text-center">
-          <ul className="text-white text-lg flex flex-col gap-4 text-left">
-            <Link>
-              <li>Customers</li>
-            </Link>
-            <hr className="opacity-50" />
-            <Link to="/employee">
-              <li className="flex gap-4 items-center hover:underline">
-                Employees
-              </li>
-            </Link>
-            <hr className="opacity-50" />
-            {/* <Link to="/document">
-              <li className="flex gap-4 items-center hover:underline">Documents</li>
-            </Link> */}
+    <div className="bg-indigo-950 text-white w-64 p-5 rounded-2xl mx-4 mb-4 flex flex-col justify-between">
+      <div className="text-4xl text-center font-bold text-sky-400">
+        <NavLink to={"/profile"}>BCMS</NavLink>
+      </div>
 
-            {/* New Doc Tab */}
-
-            <Menu as="div" className="relative inline-block text-left">
-              <Menu.Button className="inline-flex w-full justify-between text-lg items-center text-white ">
-                Documents
-                {/* <ChevronDownIcon
-                    className="-mr-1 h-5 w-5 text-white"
-                    aria-hidden="true"
-                  /> */}
-              </Menu.Button>
-
-              <Transition
-                as={Fragment}
-                enter="transition ease-out duration-100"
-                enterFrom="transform opacity-0 scale-95"
-                enterTo="transform opacity-100 scale-100"
-                leave="transition ease-in duration-75"
-                leaveFrom="transform opacity-100 scale-100"
-                leaveTo="transform opacity-0 scale-95"
+      <div>
+        <ul className="text-base font-semibold flex flex-col gap-2 text-left">
+          {/* System Management */}
+          <li className="relative">
+            <div
+              tabIndex={0}
+              onBlur={(e) => {
+                if (!e.currentTarget.contains(e.relatedTarget))
+                  setManagementDropdown(false);
+              }}
+            >
+              <button
+                onClick={toggleManagementDropdown}
+                className="flex items-center justify-between w-full px-4 py-2 rounded-2xl sidebar-hover transition-all duration-300"
               >
-                <Menu.Items className="absolute right-0 z-10 mt-2 w-full origin-top-right rounded-md bg-white font-medium shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none opacity-90">
-                  <div className="py-1">
-                    <Menu.Item>
-                      {({ active }) => (
-                        <Link
-                          to="/riskVersionControl"
-                          className={classNames(
-                            active
-                              ? "bg-gray-100 text-gray-900 "
-                              : "text-gray-700",
-                            "block px-4 py-2 text-[17px]"
-                          )}
-                        >
-                          Risk Assesements
-                        </Link>
-                      )}
-                    </Menu.Item>
-                    <Menu.Item>
-                      {({ active }) => (
-                        <Link
-                          to="/versionControls"
-                          className={classNames(
-                            active
-                              ? "bg-gray-100 text-gray-900 "
-                              : "text-gray-700",
-                            "block px-4 py-2 text-[17px]"
-                          )}
-                        >
-                          Context of the Organization
-                        </Link>
-                      )}
-                    </Menu.Item>
-                    <Menu.Item>
-                      {({ active }) => (
-                        <Link
-                          to="/bia"
-                          className={classNames(
-                            active
-                              ? "bg-gray-100 text-gray-900 "
-                              : "text-gray-700",
-                            "block px-4 py-2 text-[17px]"
-                          )}
-                        >
-                          BIA
-                        </Link>
-                      )}
-                    </Menu.Item>
-                    <Menu.Item>
-                      {({ active }) => (
-                        <Link
-                          to="/bcp"
-                          className={classNames(
-                            active
-                              ? "bg-gray-100 text-gray-900 "
-                              : "text-gray-700 ",
-                            "block px-4 py-2 text-[17px]"
-                          )}
-                        >
-                          BCP
-                        </Link>
-                      )}
-                    </Menu.Item>
-                  </div>
-                </Menu.Items>
-              </Transition>
-            </Menu>
+                System Management <FaChevronDown className="ml-2" />
+              </button>
+              {managementDropdown && (
+                <ul className="absolute left-0 w-full bg-white text-black text-sm rounded-2xl py-1 z-10">
+                  <li>
+                    <NavLink
+                      to="/employee"
+                      className={({ isActive }) =>
+                        `sidebar-link ${
+                          isActive ? "dropdown-active" : "dropdown-hover"
+                        }`
+                      }
+                      onClick={toggleManagementDropdown}
+                    >
+                      Employees
+                    </NavLink>
+                  </li>
+                  <hr className="my-1 border-black opacity-50" />
+                  <li>
+                    <NavLink
+                      to="/teams/teamList"
+                      className={({ isActive }) =>
+                        `sidebar-link ${
+                          isActive ? "dropdown-active" : "dropdown-hover"
+                        }`
+                      }
+                      onClick={toggleManagementDropdown}
+                    >
+                      Teams
+                    </NavLink>
+                  </li>
+                  <hr className="my-1 border-black opacity-50" />
+                  <li>
+                    <NavLink
+                      to="/customers"
+                      className={({ isActive }) =>
+                        `sidebar-link ${
+                          isActive ? "dropdown-active" : "dropdown-hover"
+                        }`
+                      }
+                      onClick={toggleManagementDropdown}
+                    >
+                      Customers
+                    </NavLink>
+                  </li>
+                  <hr className="my-1 border-black opacity-50" />
+                </ul>
+              )}
+            </div>
+          </li>
+          <hr className="opacity-50" />
 
-            {/* End New Doc Tab */}
+          {/* Documents */}
+          <li className="relative">
+            <div
+              tabIndex={0}
+              onBlur={(e) => {
+                if (!e.currentTarget.contains(e.relatedTarget))
+                  setDocumentDropdown(false);
+              }}
+            >
+              <button
+                onClick={toggleDocumentDropdown}
+                className="flex items-center justify-between w-full px-4 py-2 rounded-2xl sidebar-hover transition-all duration-300"
+              >
+                System Documents <FaChevronDown className="ml-2" />
+              </button>
+              {documentDropdown && (
+                <ul className="absolute left-0 w-full bg-white text-black text-sm rounded-2xl py-1 z-10">
+                  <li>
+                    <NavLink
+                      to="/Risk-Assessment/versionControl"
+                      className={({ isActive }) =>
+                        `sidebar-link ${
+                          isActive ? "dropdown-active" : "dropdown-hover"
+                        }`
+                      }
+                      onClick={toggleDocumentDropdown}
+                    >
+                      Risk Assessment
+                    </NavLink>
+                  </li>
+                  <hr className="my-1 border-black opacity-50" />
+                  <li>
+                    <NavLink
+                      to="Context-of-the-Organization/version-control"
+                      className={({ isActive }) =>
+                        `sidebar-link ${
+                          isActive ? "dropdown-active" : "dropdown-hover"
+                        }`
+                      }
+                      onClick={toggleDocumentDropdown}
+                    >
+                      Context of the Organization
+                    </NavLink>
+                  </li>
+                  <hr className="my-1 border-black opacity-50" />
+                  <li>
+                    <NavLink
+                      to="/business-continuity-plans"
+                      className={({ isActive }) =>
+                        `sidebar-link ${
+                          isActive ? "dropdown-active" : "dropdown-hover"
+                        }`
+                      }
+                      onClick={toggleDocumentDropdown}
+                    >
+                      Business Continuity Plan
+                    </NavLink>
+                  </li>
+                  <hr className="my-1 border-black opacity-50" />
+                  <li>
+                    <NavLink
+                      to="/business-impact-analysis-plans"
+                      className={({ isActive }) =>
+                        `sidebar-link ${
+                          isActive ? "dropdown-active" : "dropdown-hover"
+                        }`
+                      }
+                      onClick={toggleDocumentDropdown}
+                    >
+                      Business Impact Analysis
+                    </NavLink>
+                  </li>
+                  <hr className="my-1 border-black opacity-50" />
+                </ul>
+              )}
+            </div>
+          </li>
+          <hr className="opacity-50" />
 
-            <hr className="opacity-50" />
-            <Link to="/calendar">
-              <li className="flex gap-4 items-center hover:underline">
-                Calendar
-              </li>
-            </Link>
-            <hr className="opacity-50" />
-            <Link to="/meeting">
-              <li className="flex gap-4 items-center hover:underline">
-                Meetings
-              </li>
-            </Link>
-            <hr className="opacity-50" />
-            <Link to="/roles">
-              <li className="flex gap-4 items-center hover:underline">
-                Roles & Responsibilities
-              </li>
-            </Link>
-            <hr className="opacity-50" />
-            <Link>
-              <li>Call Tree</li>
-            </Link>
-            <hr className="opacity-50" />
-            <Link>
-              <li>Policies</li>
-            </Link>
-          </ul>
-        </div>
-        <div className="flex gap-3 mt-10 justify-center">
-          <button
-            className="px-8 py-1 bg-[#00BBF6] text-black font-semibold rounded-2xl"
-            onClick={handleLogout}
-          >
-            Logout
-          </button>
-        </div>
+          {/* Meetings */}
+          <li>
+            <NavLink
+              to="/meeting"
+              className={({ isActive }) =>
+                `sidebar-link ${isActive ? "sidebar-active" : "sidebar-hover"}`
+              }
+            >
+              Meetings
+            </NavLink>
+          </li>
+          <hr className="opacity-50" />
+
+          {/* Calendar */}
+          <li>
+            <NavLink
+              to="/calendar"
+              className={({ isActive }) =>
+                `sidebar-link ${isActive ? "sidebar-active" : "sidebar-hover"}`
+              }
+            >
+              Calendar
+            </NavLink>
+          </li>
+          <hr className="opacity-50" />
+
+          {/* Policies */}
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                `sidebar-link ${isActive ? "sidebar-active" : "sidebar-hover"}`
+              }
+            >
+              Policies and Guidelines
+            </NavLink>
+          </li>
+          <hr className="opacity-50" />
+          {/* Call Tree */}
+          <li>
+            <NavLink
+              to="/call-tree"
+              className={({ isActive }) =>
+                `sidebar-link ${isActive ? "sidebar-active" : "sidebar-hover"}`
+              }
+            >
+              Call Tree
+            </NavLink>
+          </li>
+          <hr className="opacity-50" />
+        </ul>
+      </div>
+      <div className="flex gap-3 mt-3 justify-center">
+        <button
+          className="px-8 py-1 bg-sky-400 hover:bg-sky-500 text-black font-semibold rounded-2xl"
+          onClick={handleLogout}
+        >
+          Logout
+        </button>
       </div>
     </div>
   );
