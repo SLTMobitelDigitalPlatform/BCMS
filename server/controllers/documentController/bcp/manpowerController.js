fde7ef36c4b8135b4b78561fb16fb2b3dd3df50f
Thamindu Dasanayake <136196555+thamindu2003@users.noreply.github.com>
2024-09-30 06:48:18 +0530

    Manpower update 3

--- server/controllers/documentController/bcp/manpowerController.js
+++ server/controllers/documentController/bcp/manpowerController.js
@@ -7,8 +7,12 @@ exports.getManpower = async (req, res) => {
     const manpower = await Manpower.findOne({ bcpid, option });
 
     if (!manpower) {
-      return res.status(404).json({ message: "No data found" });
+      return res.status(200).json({
+        message: "No manpower data found for this BCP.",
+        tableData: {},
+      });
     }
+
     res.json(manpower);
   } catch (err) {
     res.status(500).json({ message: "Error fetching manpower data" });
