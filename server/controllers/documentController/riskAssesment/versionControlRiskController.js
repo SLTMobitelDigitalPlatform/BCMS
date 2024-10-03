a53f6c9f4a41682347b308c96df47998de4c817a
RivinduNavanjana <ar98498@fhss.sjp.ac.lk>
2024-10-03 11:42:52 +0530

    version control notification for checked by to prepare

--- server/controllers/documentController/riskAssesment/versionControlRiskController.js
+++ server/controllers/documentController/riskAssesment/versionControlRiskController.js
@@ -1,4 +1,14 @@
 const VersionControlRisk = require("../../../models/documentModels/versionControlRiskAssModel");
+const User = require("../../../models/userModels/user");
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "sltmobitelbcmssystem@gmail.com",
+    pass: "lvbr ypwc atoa qcfi",
+  },
+});
 
 // Create a new version control
 const createVersionControl = async (req, res) => {
@@ -39,19 +49,52 @@ const getVersionById = async (req, res) => {
 // Update a versionControl by ID
 const updateVersion = async (req, res) => {
   try {
+    const { isChecked, checkedBy, approve, prepare } = req.body;
     const versionControl = await VersionControlRisk.findByIdAndUpdate(
       req.params.id,
-      req.body,
+      { isChecked, checkedBy, approve, prepare },
       { new: true }
     );
+
     if (!versionControl)
       return res.status(404).json({ message: "versionControl not found" });
+
+    // Send the response once and only once
     res.status(200).json(versionControl);
+
+    // If 'isChecked' is "Not Approved", send the email asynchronously after sending the response
+    if (isChecked === "Not Approved") {
+      const checkedByDetails = await User.find({
+        _id: { $in: prepare },
+      }).select("email name");
+
+      const checkedByEmails = checkedByDetails.map((user) => user.email);
+      const checkedByNames = checkedByDetails.map((user) => user.name).join(", ");
+
+      const mailOptions = {
+        from: "sltmobitelbcmssystem@gmail.com",
+        to: checkedByEmails,
+        subject: "Version Control",
+        text: `Dear ${checkedByNames},\n\nThe details of the event have been updated:\n
+             Title: Risk Management\n
+             Checking Status: ${isChecked}\n\n
+             Note: This is an auto-generated email. Please do not reply to this email.`,
+      };
+
+      // Send the email but do not await it, so it doesn't block or potentially cause another response
+      transporter.sendMail(mailOptions)
+        .then(info => console.log("Email sent to:", checkedByEmails.join(", "), info.response))
+        .catch(error => console.log("Error sending email:", error));
+    }
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    // If an error occurs before sending a response, catch it here and send a response
+    if (!res.headersSent) {
+      res.status(400).json({ message: error.message });
+    } else {
+      console.error("Error occurred:", error.message);
+    }
   }
 };
-
 // Delete a versionControl by ID
 const deleteVersion = async (req, res) => {
   try {
