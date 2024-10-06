7803e59dde2422f3ee8af1a280baa7ee94c226fb
Thamindu Dasanayake <136196555+thamindu2003@users.noreply.github.com>
2024-10-06 11:12:03 +0530

    Installing tanstack/react-query

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import { AuthProvider } from "./auth/AuthContext.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
+import { AuthProvider } from "./auth/AuthContext.jsx";
+import "./index.css";
 import router from "./routes.jsx";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
